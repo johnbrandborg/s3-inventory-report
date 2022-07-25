65fdec45bfd17d5c3e388707f9645ef5423eb5d5
John Brandborg <jbr@trackunit.com>
2022-07-25 14:03:20 +0200

    Change script to use S3 Inventory instead of API

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # s3-folder-size-report
-Scans an AWS Bucket and reports on the folder sizes
+Processes S3 Inventory Manifests and generates a report about the folder size and object size average
+
+
+## Setup
+
+First configure S3 to create inventory for your bucket/s.
+
+https://docs.aws.amazon.com/AmazonS3/latest/userguide/configure-inventory.html
+
+Once done you can run `s3-folder-size-report` and display the data or create a csv file.
--- s3-folder-size-report.py
+++ s3-folder-size-report.py
@@ -1,13 +1,72 @@
+#!/usr/bin/env python
+
+import csv
 from datetime import datetime
-from time import sleep
-import sys
+import gzip
+from  hashlib import md5
+from json import loads
 import os
 from threading import Thread
+from time import sleep
+import sys
+import urllib.parse
 
+from pyarrow import BufferReader, parquet
 from boto3 import client
+import botocore.exceptions
 
-inventory_file = "inventory.log"
-monitor = True
+s3 = client('s3')
+
+
+def cli():
+    bucket_name = None
+    max_depth = None
+    out_file = None
+
+    if len(sys.argv) == 1:
+        print("S3 Folder Size Report\n" + "-" * 40 + "\n",
+              "-m s3://<bucket_name>/<prefix>/\n",
+              "-d <max_depth>  Optional",
+              "-o <file> Optional")
+        return 0
+
+    for index, argv in enumerate(sys.argv):
+        if "-m" == argv:
+            manifest_location = sys.argv[index + 1]
+        if "-d" == argv:
+            try:
+                max_depth = int(sys.argv[index + 1])
+            except ValueError:
+                print("ERROR: Maximum depth needs to be an integer",
+                     file=sys.stderr)
+                return 1
+        if "-o" == argv:
+            out_file = sys.argv[index + 1]
+
+    if not manifest_location:
+        print("ERROR: An S3 location for the manifest must be supplied",
+             file=sys.stderr)
+        return 1
+
+    manifest = load_manifest(manifest_location)
+
+    try:
+        results = process_investory(manifest, max_depth)
+    except KeyboardInterrupt:
+        print("\nExiting", file=sys.stderr)
+        return 1
+
+    if out_file:
+        print(f"Writing results to out file {out_file}")
+        with open(out_file, "w") as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow(("Folder", "Size", "Count"))
+
+            for key, value in results.items():
+                record = (key, value["Size"], value["Count"])
+                writer.writerow(record)
+    else:
+        print_results(results)
 
 
 def convert_bytes(size, unit=None):
@@ -20,108 +79,96 @@ def convert_bytes(size, unit=None):
     else:
         return str(size) + ' Bytes'
 
-def create_inventory(bucket_name, prefix):
-    global monitor
+
+def load_manifest(location: str) -> dict:
+    print('Loading Inventory Manifest')
+    if not location.endswith("/"):
+        location = location + "/"
+    bucket, prefix = parse_bucket_name(location)
+
+    checksum_key = prefix + 'manifest.checksum'
+    json_key = prefix + 'manifest.json'
+
+    try:
+        manifest_json = s3.get_object(Bucket=bucket, Key=json_key).get('Body').read()
+        manifest_checksum = s3.get_object(Bucket=bucket, Key=checksum_key).get('Body').read()
+    except botocore.exceptions.ClientError as error:
+        print("ERROR:", error, file=sys.stderr)
+        sys.exit(1)
+
+    if md5(manifest_json).hexdigest() != manifest_checksum.decode().rstrip("\n"):
+        raise SystemError('The manifest failed the MD5 Check')
+
+    return loads(manifest_json)
+
+
+def process_investory(manifest, max_depth) -> dict:
+    top_level_folders = {"/": {"Size": 0, "Count": 0}}
     object_count = 0
 
-    def progress():
-        global monitor
-        reference = 0
-        while monitor:
-            if offset:= object_count - reference:
-                print("." * int(offset / 10000), end="", flush=True, file=sys.stderr)
-            reference = object_count
-            sleep(5)
-
-    s3 = client('s3')
-    paginator = s3.get_paginator('list_objects')
-    page_iterator = paginator.paginate(Bucket=bucket_name, Prefix=prefix)
-
-    if not os.path.exists(inventory_file):
-        print("Creating Inventory", file=sys.stderr)
-        Thread(target=progress).start()
-        start = datetime.now()
-        file = open(inventory_file, "w")
-        keys = []
-
-        for page in page_iterator:
-            for key in page['Contents']:
-                keys.append('"%s","%d"\n' % (key['Key'], key['Size']))
-                object_count += 1
-            
-            if len(keys) >= 10000:
-                file.writelines(keys)
-                keys.clear()
-
-        file.writelines(keys)
-        file.close()
-        duration, monitor  = (datetime.now() - start, False)
-        print("\nScanned %d objects in %d minutes" % (object_count,
-                                                      duration.seconds / 60),
-             file=sys.stderr)
-
-def process_investory(max_depth):
-    top_level_folders = {"/": 0}
-
     print("Processing Inventory", file=sys.stderr)
-    with open(inventory_file) as f:
-        for line in f.readlines():
-            fields = line.lstrip("\"").rstrip("\"\n").split("\",\"")
-            folder, size = (fields[0], int(fields[1]))
+    start = datetime.now()
+
+    for file in manifest["files"]:
+        inventory_bucket = manifest['destinationBucket'].split(":::")[1]
+        print(file['key'])
+        file_object = s3.get_object(Bucket=inventory_bucket, Key=file["key"])
+        data = file_object.get('Body').read()
+
+        if md5(data).hexdigest() != file['MD5checksum']:
+            raise SystemError('The inventory file failed the MD5 Check')
+
+        table = parquet.read_table(BufferReader(data))
+
+        for key, size in zip(table['key'], table['size']):
+            object_count += 1
+
+            folder = key.as_py()
+            size = size.as_py()
 
             folder_count = folder.count("/")
             depth = folder_count if max_depth == None or \
                 folder_count <= max_depth else max_depth
 
+            trim_base = 0
             for index in range(1, depth + 1):
-                entry = "/" + "/".join(folder.split("/")[:index]) + "/"
+                trim_point = folder.index("/", trim_base)
+                trim_base = trim_point + 1
+                entry = folder[:trim_base] 
 
-                top_level_folders["/"] += size
+                if index == 1:
+                    top_level_folders["/"]["Size"] += size
+                    top_level_folders["/"]["Count"] += 1
+
                 try:
-                    top_level_folders[entry] += size
+                    top_level_folders[entry]["Size"] += size
+                    top_level_folders[entry]["Count"] += 1
                 except KeyError:
-                    top_level_folders[entry] = size
+                    top_level_folders[entry] = {"Size": size, "Count": 1}
 
-    print("-" * 40, file=sys.stderr)
-    for folder, size in top_level_folders.items():
-        print(f"{convert_bytes(size, 'G'):>15} |", folder)
+    duration = datetime.now() - start
+    print("Processed %d objects in %d seconds\n" % (object_count,
+                                                   duration.seconds),
+         file=sys.stderr)
+
+    return top_level_folders
+
+
+def print_results(results: dict) -> None:
+    print(f"{'Total Size':>15} |{'Average':>16} |{'Count':>16} | Folder\n",
+         "-" * 80)
+
+    for folder, details in results.items():
+        print(f"{convert_bytes(details['Size'], 'G'):>15} | "
+              f"{convert_bytes(details['Size'] / details['Count'], 'K'):>15} |",
+              f"{details['Count']:>15} |",
+              urllib.parse.unquote(folder))
+
 
 def parse_bucket_name(name):
-    break_down = name.lstrip("s3://").split("/")
-    return (break_down[0], "/".join(break_down[1:]))
+    name = name.lstrip("s3://")
+    return (name[:name.index("/")], name[name.index("/"):].lstrip("/"))
 
-def cli():
-    global monitor
-    bucket_name = None
-    max_depth = None
-
-    if len(sys.argv) == 1:
-        print("S3 Folder Size Report\n" + "-" * 40 + "\n",
-              "-b <bucket_name>/<prefix>\n",
-              "-d <max_depth>  Optional")
-        return 1
-
-    for index, argv in enumerate(sys.argv):
-        if "-b" == argv:
-            bucket_name, prefix = parse_bucket_name(sys.argv[index + 1])
-        if "-d" == argv:
-            try:
-                max_depth = int(sys.argv[index + 1])
-            except ValueError:
-                print("ERROR: Maximum depth needs to be an integer")
-                return 1
-
-    if not bucket_name:
-        print("ERROR: A bucket name must be supplied")
-        return 1
-
-    try:
-        create_inventory(bucket_name, prefix)
-        process_investory(max_depth)
-    except KeyboardInterrupt:
-        print("\nExiting", file=sys.stderr)
-        monitor = False
-        return 1
 
 if __name__ == "__main__":
     sys.exit(cli())
